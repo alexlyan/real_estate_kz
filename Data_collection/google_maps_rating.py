07c3e1c09a39604bec2462fe77a5839186076976
Alex <alexlyan@yahoo.com>
2020-07-18 21:09:45 +0600

    added code to get ratings of places

---
+++ Data_collection/google_maps_rating.py
@@ -0,0 +1,84 @@
+import requests
+import json
+import numpy as np
+
+class GoogleAPI(object):
+    def __init__(self, api):
+        self.api = api
+    
+    def getting_ratings_by_coordinates(self, location, radius, types):
+        
+        endpoint_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+
+        params = {
+            'location': location,
+            'radius': radius,
+            'types': types,
+            'key': self.api
+        }
+
+        request = requests.get(endpoint_url, params)
+
+        json_data = json.loads(request.content)
+
+        def overall_rating(x): 
+            try: 
+                return x['rating'] 
+            except KeyError: 
+                return None 
+
+        mean_rating = np.mean([overall_rating(x) for x in json_data['results'] if overall_rating(x) is not None]) 
+
+        return mean_rating
+
+    
+    def getting_price_level_by_coordinates(self, location, radius, types):
+        
+        endpoint_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+
+        params = {
+            'location': location,
+            'radius': radius,
+            'types': types,
+            'key': self.api
+        }
+
+        request = requests.get(endpoint_url, params)
+
+        json_data = json.loads(request.content)
+
+        def price_level(x): 
+            try: 
+                return x['price_level'] 
+            except KeyError: 
+                return None 
+
+        price_rating = np.mean([price_level(x) for x in json_data['results'] if price_level(x) is not None]) 
+
+        return price_rating
+
+
+    def getting_shopping_mall_by_coordinates(self, location, radius, types):
+        
+        endpoint_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+
+        params = {
+            'location': location,
+            'radius': radius,
+            'types': types,
+            'key': self.api
+        }
+
+        request = requests.get(endpoint_url, params)
+
+        json_data = json.loads(request.content)
+
+        def shop_rating(x): 
+            try: 
+                return x['shopping_mall'] 
+            except KeyError: 
+                return None 
+
+        shop_rating = np.mean([shop_rating(x) for x in json_data['results'] if shop_rating(x) is not None]) 
+
+        return shop_rating
